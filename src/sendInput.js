ccaafa95f03d4dde32e39e0bb8c12abd6c4d7570
RonnieArnab <as920037.arnabghosh@gmail.com>
2023-11-17 19:26:24 +0530

    changed the url

--- src/sendInput.js
+++ src/sendInput.js
@@ -1,12 +1,15 @@
 const setintoBackend = async (inputData) => {
   try {
-    const response = await fetch("http://127.0.0.1:5000/api/members", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(inputData),
-    });
+    const response = await fetch(
+      "https://online-fraud-detection-backend-9ejy-ten.vercel.app/api/members",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputData),
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
